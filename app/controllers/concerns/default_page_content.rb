0a80deb3ad9aa0d47a3129cdae16e8bc8cb38eba
creyes-dev <creyes.dev@gmail.com>
2021-04-01 11:38:14 -0700

    dynamic page title and SEO keyboards

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+    extend ActiveSupport::Concern
+
+    included do
+        before_filter :set_page_defaults
+    end
+
+    def set_page_defaults
+        @page_title = "DevcampPortfolio | My portfolio app"
+        @seo_keyboards = "creyes-dev portfolio"
+    end
+end
