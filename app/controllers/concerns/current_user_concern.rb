a4ddc3a6f428fae418f7a42a12b77921c3ca6824
creyes-dev <creyes.dev@gmail.com>
2021-04-07 20:24:37 -0700

    using GuestUser class instead of OpenStruct

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,6 +2,13 @@ module CurrentUserConcern
     extend ActiveSupport::Concern
     # Overriding devise current_user
     def current_user
-        super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+        guest = GuestUser.new
+        guest.name = "Guest User"
+        guest.first_name = "Guest"
+        guest.last_name = "User"
+        guest.email = "guest@example.com"
+        guest
     end
+
+
 end
