86abecb9fd69e7fff1e90bb7479775da9c531f7c
creyes-dev <creyes.dev@gmail.com>
2021-04-08 18:00:44 -0700

    fixing current_user

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,6 +2,10 @@ module CurrentUserConcern
     extend ActiveSupport::Concern
     # Overriding devise current_user
     def current_user
+        super || guest_user
+    end
+
+    def guest_user
         guest = GuestUser.new
         guest.name = "Guest User"
         guest.first_name = "Guest"
