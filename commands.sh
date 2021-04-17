0bf2475ee1bdb5fa63dc0f1a29d2e519bdfb3e24
creyes-dev <creyes.dev@gmail.com>
2021-04-17 12:13:39 -0700

    several blog layout changes

--- commands.sh
+++ commands.sh
@@ -974,3 +974,186 @@ $ rails db:migrate
 -- add_column(:users, :roles, :string)
    -> 0.0194s
 == 20210408001737 AddRolesToUsers: migrated (0.0194s) =========================
+
+$ bundle install
+Fetching gem metadata from https://rubygems.org/............
+Fetching gem metadata from https://rubygems.org/.
+Resolving dependencies...
+Using rake 13.0.3
+Using concurrent-ruby 1.1.8
+Using i18n 1.8.9
+Using minitest 5.14.4
+Using thread_safe 0.3.6
+Using tzinfo 1.2.9
+Using activesupport 5.0.7.2
+Using builder 3.2.4
+Using erubis 2.7.0
+Using mini_portile2 2.4.0
+Using nokogiri 1.10.10
+Using rails-dom-testing 2.0.3
+Using crass 1.0.6
+Using loofah 2.9.0
+Using rails-html-sanitizer 1.3.0
+Using actionview 5.0.7.2
+Using rack 2.2.3
+Using rack-test 0.6.3
+Using actionpack 5.0.7.2
+Using nio4r 2.5.7
+Using websocket-extensions 0.1.5
+Using websocket-driver 0.6.5
+Using actioncable 5.0.7.2
+Using globalid 0.4.2
+Using activejob 5.0.7.2
+Using mini_mime 1.0.2
+Using mail 2.7.1
+Using actionmailer 5.0.7.2
+Using activemodel 5.0.7.2
+Using arel 7.1.4
+Using activerecord 5.0.7.2
+Using execjs 2.7.0
+Using autoprefixer-rails 10.2.4.0
+Using bcrypt 3.1.16
+Using bindex 0.8.1
+Using popper_js 1.16.0
+Using rb-fsevent 0.10.4
+Using ffi 1.15.0
+Using rb-inotify 0.10.1
+Using sass-listen 4.0.0
+Using sass 3.7.4
+Using bootstrap 4.0.0
+Using bundler 1.17.3
+Using byebug 11.1.3
+Using coffee-script-source 1.12.2
+Using coffee-script 2.4.1
+Using method_source 1.0.0
+Using thor 1.1.0
+Using railties 5.0.7.2
+Using coffee-rails 4.2.2
+Using orm_adapter 0.5.0
+Using responders 3.0.1
+Using warden 1.2.9
+Using devise 4.7.3
+Fetching font-awesome-rails 4.7.0.7
+Installing font-awesome-rails 4.7.0.7
+Using friendly_id 5.1.0
+Using jbuilder 2.11.2
+Using jquery-rails 4.4.0
+Using listen 3.0.8
+Using petergate 1.9.1
+Using pg 0.21.0
+Using puma 3.12.6
+Using sprockets 3.7.2
+Using sprockets-rails 3.2.2
+Using rails 5.0.7.2
+Using tilt 2.0.10
+Using sass-rails 5.0.7
+Using spring 2.1.1
+Using spring-watcher-listen 2.0.1
+Using turbolinks-source 5.2.0
+Using turbolinks 5.2.1
+Using uglifier 4.2.0
+Using web-console 3.7.0
+Bundle complete! 20 Gemfile dependencies, 73 gems now installed.
+Use `bundle info [gemname]` to see where a bundled gem is installed.
+
+# Installing kaminari
+
+$ bundle install
+Fetching gem metadata from https://rubygems.org/............
+Fetching gem metadata from https://rubygems.org/.
+Resolving dependencies...
+Using rake 13.0.3
+Using concurrent-ruby 1.1.8
+Using i18n 1.8.9
+Using minitest 5.14.4
+Using thread_safe 0.3.6
+Using tzinfo 1.2.9
+Using activesupport 5.0.7.2
+Using builder 3.2.4
+Using erubis 2.7.0
+Using mini_portile2 2.4.0
+Using nokogiri 1.10.10
+Using rails-dom-testing 2.0.3
+Using crass 1.0.6
+Using loofah 2.9.0
+Using rails-html-sanitizer 1.3.0
+Using actionview 5.0.7.2
+Using rack 2.2.3
+Using rack-test 0.6.3
+Using actionpack 5.0.7.2
+Using nio4r 2.5.7
+Using websocket-extensions 0.1.5
+Using websocket-driver 0.6.5
+Using actioncable 5.0.7.2
+Using globalid 0.4.2
+Using activejob 5.0.7.2
+Using mini_mime 1.0.2
+Using mail 2.7.1
+Using actionmailer 5.0.7.2
+Using activemodel 5.0.7.2
+Using arel 7.1.4
+Using activerecord 5.0.7.2
+Using execjs 2.7.0
+Using autoprefixer-rails 10.2.4.0
+Using bcrypt 3.1.16
+Using bindex 0.8.1
+Using popper_js 1.16.0
+Using rb-fsevent 0.10.4
+Using ffi 1.15.0
+Using rb-inotify 0.10.1
+Using sass-listen 4.0.0
+Using sass 3.7.4
+Using bootstrap 4.0.0
+Using bundler 1.17.3
+Using byebug 11.1.3
+Using coffee-script-source 1.12.2
+Using coffee-script 2.4.1
+Using method_source 1.0.0
+Using thor 1.1.0
+Using railties 5.0.7.2
+Using coffee-rails 4.2.2
+Using orm_adapter 0.5.0
+Using responders 3.0.1
+Using warden 1.2.9
+Using devise 4.7.3
+Using font-awesome-rails 4.7.0.7
+Using friendly_id 5.1.0
+Using jbuilder 2.11.2
+Using jquery-rails 4.4.0
+Fetching kaminari-core 1.2.1
+Installing kaminari-core 1.2.1
+Fetching kaminari-actionview 1.2.1
+Installing kaminari-actionview 1.2.1
+Fetching kaminari-activerecord 1.2.1
+Installing kaminari-activerecord 1.2.1
+Fetching kaminari 1.2.1
+Installing kaminari 1.2.1
+Using listen 3.0.8
+Using petergate 1.9.1
+Using pg 0.21.0
+Using puma 3.12.6
+Using sprockets 3.7.2
+Using sprockets-rails 3.2.2
+Using rails 5.0.7.2
+Using tilt 2.0.10
+Using sass-rails 5.0.7
+Using spring 2.1.1
+Using spring-watcher-listen 2.0.1
+Using turbolinks-source 5.2.0
+Using turbolinks 5.2.1
+Using uglifier 4.2.0
+Using web-console 3.7.0
+Bundle complete! 21 Gemfile dependencies, 77 gems now installed.
+Use `bundle info [gemname]` to see where a bundled gem is installed.
+
+# Configuring kaminari
+
+$ rails generate kaminari:views github
+
+      downloading app/views/kaminari/_next_page.html.erb from kaminari_themes...
+      create  app/views/kaminari/_next_page.html.erb
+      downloading app/views/kaminari/_paginator.html.erb from kaminari_themes...
+      create  app/views/kaminari/_paginator.html.erb
+      downloading app/views/kaminari/_prev_page.html.erb from kaminari_themes...
+      create  app/views/kaminari/_prev_page.html.erb
+
